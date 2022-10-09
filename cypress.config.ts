a1dd512c0980a9cf3c2cc1707a09b47c201ce58b
Decker Urbano <decker.urbano@geopagos.com>
2022-10-09 01:27:18 -0300

    feat: add cypress

---
+++ cypress.config.ts
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+    e2e: {
+        baseUrl: 'http://localhost:3000',
+        video:false,
+        screenshotOnRunFailure:false
+    },
+})
+
+export default {}
